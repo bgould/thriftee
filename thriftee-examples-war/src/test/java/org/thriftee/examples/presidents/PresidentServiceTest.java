aa32339dda6ee203af73d18045bbf2e9b32cc5b0
BCG <bcg@example.com>
2015-04-30 14:24:05 -0400

    cleaning up temp dirs

--- thriftee-examples-war/src/test/java/org/thriftee/examples/presidents/PresidentServiceTest.java
+++ thriftee-examples-war/src/test/java/org/thriftee/examples/presidents/PresidentServiceTest.java
@@ -29,7 +29,8 @@ public class PresidentServiceTest {
         StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[2];
         String className = stackTraceElement.getClassName();
         String simpleName = className.substring(className.lastIndexOf('.') + 1);
-        File tempDir = new File("target/tests/" + simpleName);
+        String prefix = System.getProperty("thriftee.build.dir", "target");
+        File tempDir = new File(prefix + "/tests/" + simpleName);
         File retval = new File(tempDir, stackTraceElement.getMethodName());
         return retval;
     }
