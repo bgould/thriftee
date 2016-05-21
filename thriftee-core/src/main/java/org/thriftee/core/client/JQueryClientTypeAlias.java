cd9096f2d49d34fa3566766ea61b469978a83cc2
BCG <bgould@users.noreply.github.com>
2016-05-21 13:28:39 -0400

    Fixed ordering of concatenated file in jquery client

--- thriftee-core/src/main/java/org/thriftee/core/client/JQueryClientTypeAlias.java
+++ thriftee-core/src/main/java/org/thriftee/core/client/JQueryClientTypeAlias.java
@@ -31,6 +31,8 @@ import java.io.Writer;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -49,17 +51,17 @@ import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 public class JQueryClientTypeAlias extends ClientTypeAlias {
 
   public static final String CONCAT_FILE_NAME = "client-jquery-all.js";
-  
+
   public static final String CONCAT_FILE_MIN_NAME = "client-jquery-all.min.js";
 
   public static final String JQUERY_FILE_NAME = "jquery.min.js";
-  
+
   public static final String THRIFTEE_JS_NAME = "thriftee.js";
 
   public static final String THRIFTEE_ALL_JS = "thriftee-all.js";
 
   public static final String THRIFTEE_ALL_MIN_JS = "thriftee-all.min.js";
-  
+
   public static final String THRIFT_JS_NAME = "thrift.js";
 
   public static final String JS_PATH = "META-INF/thriftee/clients/jquery/";
@@ -77,29 +79,49 @@ public class JQueryClientTypeAlias extends ClientTypeAlias {
     final File libDir = event.getThriftLibDir();
     LOG.debug("Post processing jQuery client library: {}", dir);
     LOG.debug("libDir: {}", libDir.getAbsolutePath());
-    
-    final String[] gen = dir.list();
-    final String[] all = new String[] { 
+
+    // need to make sure that the *_types.js are included before the services
+    final List<String> sorted = new ArrayList<>(Arrays.asList(dir.list()));
+    Collections.sort(sorted, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        if (o1 == null) throw new IllegalArgumentException("o1 cannot be null");
+        if (o2 == null) throw new IllegalArgumentException("o2 cannot be null");
+        boolean o1_types = o1.endsWith("_types.js");
+        boolean o2_types = o2.endsWith("_types.js");
+        if (o1_types == o2_types) {
+          return o1.compareTo(o2);
+        } else if (o1_types) {
+          return -1;
+        } else if (o2_types) {
+          return 1;
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+    });
+    final String[] gen = sorted.toArray(new String[sorted.size()]);
+    final String[] all = new String[] {
       THRIFT_JS_NAME,
-      CONCAT_FILE_NAME, 
+      CONCAT_FILE_NAME,
       THRIFTEE_JS_NAME,
     };
-   
+
     final File thriftSrc = new File(libDir, "js/src/" + THRIFT_JS_NAME);
     FileUtil.copyFile(thriftSrc, new File(dir, THRIFT_JS_NAME));
     copyJs(dir, JQUERY_FILE_NAME);
     copyJs(dir, THRIFTEE_JS_NAME);
-    
+
     concatenate(dir, CONCAT_FILE_NAME, gen);
     compressJs(dir, CONCAT_FILE_NAME, CONCAT_FILE_MIN_NAME);
-    
+
     concatenate(dir, THRIFTEE_ALL_JS, all);
     compressJs(dir, THRIFTEE_ALL_JS, THRIFTEE_ALL_MIN_JS);
   }
 
   private void concatenate(
-      final File dir, 
-      final String outputFilename, 
+      final File dir,
+      final String outputFilename,
       final String[] dirList) throws IOException {
     final File outputFile = new File(dir, outputFilename);
     if (outputFile.exists()) {
@@ -125,18 +147,6 @@ public class JQueryClientTypeAlias extends ClientTypeAlias {
           }
         }
       }
-      /*
-      try (final OutputStreamWriter fw = new OutputStreamWriter(fileOut)) {
-        try (final PrintWriter pw = new PrintWriter(fw)) {
-          for (final File file : files) {
-            LOG.debug("  {}", file.getName());
-            final String content = FileUtil.readAsString(file, "UTF-8");
-            pw.println(content);
-            pw.flush();
-          }        
-        }
-      }
-      */
     }
     LOG.debug("--- finished {} ---", outputFile.getName());
   }
@@ -209,10 +219,10 @@ public class JQueryClientTypeAlias extends ClientTypeAlias {
       private String buildMsg(
           String msg, String src, int line, String lineSrc, int offset) {
         return String.format(
-          "  source = %s,%n" + 
+          "  source = %s,%n" +
           "  lineSource = %s,%n" +
-          "  line = %s,%n" + 
-          "  offset = %s,%n" + 
+          "  line = %s,%n" +
+          "  offset = %s,%n" +
           "  message = %s%n",
           src, lineSrc, line, offset, msg
         );
